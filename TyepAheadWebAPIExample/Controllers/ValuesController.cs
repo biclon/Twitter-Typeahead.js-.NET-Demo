26638b42f5f62489bf84bc9e335985922a1dc331
biclon <lonny.bice@gmail.com>
2015-02-27 15:19:27 -0500

    Initial Commit
    Recommitting the initial commit with the correct profile/settings in the
    git client.

---
+++ TyepAheadWebAPIExample/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using TyepAheadWebAPIExample.Models;
+
+namespace TyepAheadWebAPIExample.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values/GetAllBooks
+        /// <summary>
+        /// This method is going to be used for the prefetching of all data used for the Twitter typeahead
+        /// </summary>
+        /// <returns>A list of books that will be serialized to JSON and used by the Twitter typeahead</returns>
+        public IEnumerable<Book> GetAllBooks()
+        {
+            List<Book> books = GetBooks();
+
+            return books;
+        }
+
+        // GET api/values/SearchBooks?searchTerm={%QUERY}
+        /// <summary>
+        /// This method will be used to handle searching for books
+        /// </summary>
+        /// <param name="searchTerm">The term being searched for. In this case, either an author or a book title.</param>
+        /// <returns>A list of books matching the searched term.</returns>
+        [HttpGet]
+        public IEnumerable<Book> SearchBooks(string searchTerm)
+        {
+            searchTerm = searchTerm.ToUpper();
+            IEnumerable<Book> books = GetAllBooks().Where(x=>x.Title.ToUpper().Contains(searchTerm) || x.Author.ToUpper().Contains(searchTerm));
+
+            return books;
+        }
+
+        private List<Book> GetBooks()
+        {
+            List<Book> books = new List<Book>();
+
+            if (HttpContext.Current.Cache["BooksList"] != null)
+            {
+                books = (List<Book>)HttpContext.Current.Cache["BooksList"];
+            }
+            else
+            {
+                books.Add(new Book()
+                {
+                    Author = "Bret Easton Ellis",
+                    Title = "American Psycho"
+                });
+
+                books.Add(new Book()
+                {
+                    Author = "Bret Easton Ellis",
+                    Title = "Less Than Zero"
+                });
+
+                books.Add(new Book()
+                {
+                    Title = "The Winner",
+                    Author = "David Baldacci"
+                });
+
+                books.Add(new Book()
+                {
+                    Title = "Private Vegas",
+                    Author = "James Patterson"
+                });
+            }
+
+            return books;
+        }
+    }
+}
---
+++ TyepAheadWebAPIExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TyepAheadWebAPIExample.WebApiApplication" Language="C#" %>
